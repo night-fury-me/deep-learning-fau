12a2ed2c26c38ae9fca29fdf0b8d3aee35872f58
night-fury-me <karim.redwanul@outlook.com>
2024-05-29 01:14:32 +0200

    Fixed bug in the FC layer.

--- Exercises/Exercise-01/src_to_implement/Layers/FullyConnected.py
+++ Exercises/Exercise-01/src_to_implement/Layers/FullyConnected.py
@@ -8,7 +8,7 @@ class FullyConnected(BaseLayer):
         self.trainable  = True
         self.weights    = np.random.uniform(0, 1, (input_size + 1, output_size)) # [5 x 3]
         
-        self.optimizer: BaseOptimizer   = None
+        self._optimizer: BaseOptimizer  = None
         self.delta_w                    = None
 
     def forward(self, input_tensor):
@@ -22,27 +22,27 @@ class FullyConnected(BaseLayer):
         
         # delta_a^(L-1) = w^L * activation_fn(z^L) * 2(a^L - y) 
         # w^L == weights && activation_fn(z^L) * 2(a^L - y) == error_tensor 
-        gradient_input = np.dot(error_tensor, weights.T)    # [9 x 4]
+        next_layer_gradients = np.dot(error_tensor, weights.T)    # [9 x 4]
 
         # delta_w = a^(L-1) * activation_fn(z^L) * 2(a^L - y)
         # a^(L-1) == self.input_tensor && activation_fn(z^L) * 2(a^L - y) == error_tensor
         self.delta_w = np.dot(self.input_tensor.T, error_tensor) # [5 x 3] 
 
-        if self.optimizer:
-            self.weights = self.optimizer.calculate_update(
+        if self._optimizer:
+            self.weights = self._optimizer.calculate_update(
                 weight_tensor   = self.weights, 
                 gradient_tensor = self.delta_w
             ) # [5 x 3]
 
-        return gradient_input
+        return next_layer_gradients
 
     @property
     def optimizer(self):
-        return self.optimizer
+        return self._optimizer
     
     @optimizer.setter
     def optimizer(self, optimizer):
-        self.optimizer = optimizer
+        self._optimizer = optimizer
     
     @property
     def gradient_weights(self):
