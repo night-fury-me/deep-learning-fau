e68873a29f16d27b3e38b3570125e8b687fe0802
night-fury-me <karim.redwanul@outlook.com>
2024-05-29 01:22:25 +0200

    Updated submission files.

--- Exercises/Exercise-01/submission/FullyConnected.py
+++ Exercises/Exercise-01/submission/FullyConnected.py
@@ -1,14 +1,15 @@
 import numpy as np # type: ignore
 from Layers.Base import BaseLayer
-from Optimization import Optimizers
+from Optimization.Optimizers import BaseOptimizer
 
 class FullyConnected(BaseLayer):
     def __init__(self, input_size, output_size) -> None:
         super().__init__()
-        self.trainable = True
-        self.weights = np.random.uniform(0, 1, (input_size + 1, output_size)) # [5 x 3]
-        self._optimizer: Optimizers.BaseOptimizer = None
-        self._gradient_weights = None
+        self.trainable  = True
+        self.weights    = np.random.uniform(0, 1, (input_size + 1, output_size)) # [5 x 3]
+        
+        self._optimizer: BaseOptimizer  = None
+        self.delta_w                    = None
 
     def forward(self, input_tensor):
         initial_bias = np.ones((input_tensor.shape[0], 1))
@@ -18,17 +19,22 @@ class FullyConnected(BaseLayer):
     def backward(self, error_tensor):
         # error_tensor -> [9 x 3]
         weights = self.weights[:self.weights.shape[0]-1, :] # [4 x 3]: bias excluded
-        gradient_input = np.dot(error_tensor, weights.T)    # [9 x 4]
+        
+        # delta_a^(L-1) = w^L * activation_fn(z^L) * 2(a^L - y) 
+        # w^L == weights && activation_fn(z^L) * 2(a^L - y) == error_tensor 
+        next_layer_gradients = np.dot(error_tensor, weights.T)    # [9 x 4]
 
-        self._gradient_weights = np.dot(self.input_tensor.T, error_tensor) # [5 x 3] 
+        # delta_w = a^(L-1) * activation_fn(z^L) * 2(a^L - y)
+        # a^(L-1) == self.input_tensor && activation_fn(z^L) * 2(a^L - y) == error_tensor
+        self.delta_w = np.dot(self.input_tensor.T, error_tensor) # [5 x 3] 
 
         if self._optimizer:
             self.weights = self._optimizer.calculate_update(
-                self.weights, 
-                self._gradient_weights
+                weight_tensor   = self.weights, 
+                gradient_tensor = self.delta_w
             ) # [5 x 3]
 
-        return gradient_input
+        return next_layer_gradients
 
     @property
     def optimizer(self):
@@ -40,4 +46,4 @@ class FullyConnected(BaseLayer):
     
     @property
     def gradient_weights(self):
-        return self._gradient_weights
+        return self.delta_w
--- Exercises/Exercise-01/submission/SoftMax.py
+++ Exercises/Exercise-01/submission/SoftMax.py
@@ -8,11 +8,15 @@ class SoftMax(BaseLayer):
         super().__init__()
     
     def forward(self, input_tensor):
-        scaled_input = input_tensor - np.max(input_tensor, axis = 1, keepdims = True)
+        # if x_k > 0 --> e^(x_k) might become very large
+        # So, shifting to increase numerical stability: x_hat = x - max(x)
+        scaled_input = input_tensor - np.max(input_tensor, axis = 1, keepdims = True) 
+
         exps = np.exp(scaled_input) 
-        self.output_tensor = exps / np.sum(exps, axis = 1, keepdims = True)
-        return self.output_tensor
+        self.y_hat = exps / np.sum(exps, axis = 1, keepdims = True)
+        return self.y_hat
     
     def backward(self, error_tensor):
-        return self.output_tensor * (
-            error_tensor - (error_tensor * self.output_tensor).sum(axis = 1).reshape(-1, 1))
+        # E_(n-1) = y_hat * (E_n - E_n * y_hat)
+        return self.y_hat * (
+            error_tensor - (error_tensor * self.y_hat).sum(axis = 1).reshape(-1, 1))
