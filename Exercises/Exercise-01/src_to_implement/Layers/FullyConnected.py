a23a414bc784026bdf2f5fbd93947921c5ed8e2e
night-fury-me <karim.redwanul@outlook.com>
2024-05-29 01:09:01 +0200

    Added comments with formula.

--- Exercises/Exercise-01/src_to_implement/Layers/FullyConnected.py
+++ Exercises/Exercise-01/src_to_implement/Layers/FullyConnected.py
@@ -9,7 +9,7 @@ class FullyConnected(BaseLayer):
         self.weights    = np.random.uniform(0, 1, (input_size + 1, output_size)) # [5 x 3]
         
         self._optimizer: BaseOptimizer  = None
-        self._gradient_weights          = None
+        self.delta_w          = None
 
     def forward(self, input_tensor):
         initial_bias = np.ones((input_tensor.shape[0], 1))
@@ -19,14 +19,19 @@ class FullyConnected(BaseLayer):
     def backward(self, error_tensor):
         # error_tensor -> [9 x 3]
         weights = self.weights[:self.weights.shape[0]-1, :] # [4 x 3]: bias excluded
+        
+        # delta_a^(L-1) = w^L * activation_fn(z^L) * 2(a^L - y) 
+        # w^L == weights && activation_fn(z^L) * 2(a^L - y) == error_tensor 
         gradient_input = np.dot(error_tensor, weights.T)    # [9 x 4]
 
-        self._gradient_weights = np.dot(self.input_tensor.T, error_tensor) # [5 x 3] 
+        # delta_w = a^(L-1) * activation_fn(z^L) * 2(a^L - y)
+        # a^(L-1) == self.input_tensor && activation_fn(z^L) * 2(a^L - y) == error_tensor
+        self.delta_w = np.dot(self.input_tensor.T, error_tensor) # [5 x 3] 
 
         if self._optimizer:
             self.weights = self._optimizer.calculate_update(
-                self.weights, 
-                self._gradient_weights
+                weight_tensor   = self.weights, 
+                gradient_tensor = self.delta_w
             ) # [5 x 3]
 
         return gradient_input
@@ -41,4 +46,4 @@ class FullyConnected(BaseLayer):
     
     @property
     def gradient_weights(self):
-        return self._gradient_weights
+        return self.delta_w
